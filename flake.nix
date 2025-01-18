fcf3751ee34b152608dbf8b14a9c3557e4dcbd05
System administrator <websiteaccforme@gmail.com>
2025-01-18 11:31:38 -0500

    commit

--- flake.nix
+++ flake.nix
@@ -5,11 +5,31 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }: 
+  let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+    };
+  in
+  {
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    packages.default = pkgs.mkshell {
+      buildInputs = [
+        pkgs.python310
+        pkgs.git        
+        pkgs.docker
+      ];
+    };
+    devShells.default = pkgs.mkshell {
+      buildInputs = [
+        pkgs.python310
+        pkgs.git
+        pkgs.docker 
+      ];
+    };
 
   };
 }
