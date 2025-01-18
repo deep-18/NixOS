15ff862eb260388fa9d7779a3e29ce75187a134b
Deep Raval <60513881+deep-18@users.noreply.github.com>
2025-01-18 13:40:15 -0500

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -102,7 +102,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  #services.openssh.enable = true;
+  services.openssh.enable = true;
   
   #Show Cat Image
   services.nginx={
